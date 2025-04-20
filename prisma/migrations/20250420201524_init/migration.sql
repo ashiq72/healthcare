4eaf1cfb3ff84407332b4ca3fdd03fb34977bb64
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-04-21 03:29:49 +0600

    Updated

---
+++ prisma/migrations/20250420201524_init/migration.sql
@@ -0,0 +1,68 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('user', 'admin');
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "post_caegory" (
+    "postId" INTEGER NOT NULL,
+    "categorId" INTEGER NOT NULL,
+
+    CONSTRAINT "post_caegory_pkey" PRIMARY KEY ("postId","categorId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_userId_key" ON "profiles"("userId");
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_caegory" ADD CONSTRAINT "post_caegory_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_caegory" ADD CONSTRAINT "post_caegory_categorId_fkey" FOREIGN KEY ("categorId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/create.ts
+++ src/create.ts
@@ -1,15 +1,49 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-  const result = await prisma.post.create({
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     username: "Atik",
+  //     email: "atik@gmail.com",
+  //     role: UserRole.user,
+  //   },
+  // });
+
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "This is bio",
+  //     userId: 4,
+  //   },
+  // })
+
+  // const createCategory = await prisma.category.create({
+  //   data: {
+  //     name: "Category 1",
+  //   },
+  // });
+  const createPost = await prisma.post.create({
     data: {
-      title: "Ashiq focusing Prisma 41",
-      content: "This is content 41",
+      title: "Category 1",
+      content: "This is content",
+      authorId: 1,
+      postCategory: {
+        create: {
+          // categoryId: 3
+          category: {
+            connect: {
+              id: 1,
+            },
+          },
+        },
+      },
+    },
+    include: {
+      postCategory: true,
     },
   });
-
+  console.log(createPost);
   // const createMany = await prisma.post.createMany({
   //   data: [
   //     {
@@ -19,123 +53,9 @@ const main = async () => {
   //     {
   //       title: "Ashiq focusing Prisma 10",
   //       content: "This is content 10",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 11",
-  //       content: "This is content 11",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 12",
-  //       content: "This is content 12",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 13",
-  //       content: "This is content 13",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 14",
-  //       content: "This is content 14",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 15",
-  //       content: "This is content 15",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 16",
-  //       content: "This is content 16",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 17",
-  //       content: "This is content 17",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 18",
-  //       content: "This is content 18",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 19",
-  //       content: "This is content 19",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 20",
-  //       content: "This is content 20",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 21",
-  //       content: "This is content 21",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 22",
-  //       content: "This is content 22",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 23",
-  //       content: "This is content 23",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 24",
-  //       content: "This is content 24",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 25",
-  //       content: "This is content 25",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 26",
-  //       content: "This is content 26",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 27",
-  //       content: "This is content 27",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 28",
-  //       content: "This is content 28",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 29",
-  //       content: "This is content 29",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 30",
-  //       content: "This is content 30",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 31",
-  //       content: "This is content 31",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 32",
-  //       content: "This is content 32",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 33",
-  //       content: "This is content 33",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 34",
-  //       content: "This is content 34",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 35",
-  //       content: "This is content 35",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 36",
-  //       content: "This is content 36",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 37",
-  //       content: "This is content 37",
-  //     },
-  //     {
-  //       title: "Ashiq focusing Prisma 38",
-  //       content: "This is content 38",
-  //     },
+  //     }
   //   ],
   // });
-
-  // console.log(createMany);
 };
 
 main();
